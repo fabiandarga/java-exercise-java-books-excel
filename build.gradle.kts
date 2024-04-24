3ecb9ed98f4a31574bd25e9772e7636533e6f03f
Fabian Darga <kontakt@fabiandarga.de>
2024-04-24 14:42:48 +0200

    Add dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,14 +5,19 @@ plugins {
 group = "org.example"
 version = "1.0-SNAPSHOT"
 
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(11))
+    }
+}
+
 repositories {
     mavenCentral()
 }
 
 dependencies {
     implementation("org.apache.poi:poi:5.2.5")
-}
-
-tasks.test {
-    useJUnitPlatform()
+    implementation("org.apache.poi:poi-ooxml:5.2.5")
+    implementation("org.apache.poi:poi-ooxml-lite:5.2.5")
+    implementation("org.apache.logging.log4j:log4j-core:2.23.1")
 }
